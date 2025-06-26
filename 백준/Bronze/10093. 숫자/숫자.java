ec6e384b519c2b7b0e888609297d2bc72b5ca206
w00jinLee <122732558+w00jinLee@users.noreply.github.com>
2025-06-26 19:10:28 +0900

    [Bronze II] Title: 숫자, Time: 1188 ms, Memory: 34072 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+ 
+class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in); 
+       long a=sc.nextLong();
+       long b=sc.nextLong();
+       
+       if(a>b) {
+           System.out.println(a-b-1);
+           for(long i=b+1;i<a;i++) {
+               System.out.print(i+" ");
+           }
+       }
+       if(b>a) {
+           System.out.println(b-a-1);
+           for(long i=a+1;i<b;i++) {
+               System.out.print(i+" ");
+           }
+       }
+       if(a==b) {
+           System.out.println(0);
+       }
+    }
+}
