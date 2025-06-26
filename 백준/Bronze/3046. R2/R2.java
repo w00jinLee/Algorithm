7a72cbdc95f278c9b73b2dde800ba4f35bbf817d
w00jinLee <122732558+w00jinLee@users.noreply.github.com>
2025-06-26 19:13:24 +0900

    [Bronze IV] Title: R2, Time: 204 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	int r1 = sc.nextInt();
+	int s = sc.nextInt();
+	
+	System.out.print(2*s - r1);
+	}
+}
