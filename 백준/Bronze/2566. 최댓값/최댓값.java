aa623b5c4112413f467f406474208da978847d1d
w00jinLee <122732558+w00jinLee@users.noreply.github.com>
2025-06-26 18:41:56 +0900

    [Bronze III] Title: 최댓값, Time: 196 ms, Memory: 18456 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[9][9];
+        int max = 0;
+        int x=1;
+        int y=1;
+
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(arr[i][j]>max){
+                    max=arr[i][j];
+                    x=i+1; 
+                    y=j+1;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.print(x+" "+y);
+    }
+}
