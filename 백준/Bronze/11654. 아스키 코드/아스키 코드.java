604672714ac9cf6926c416625a1da5ca1473deac
w00jinLee <122732558+w00jinLee@users.noreply.github.com>
2025-06-26 18:45:59 +0900

    [Bronze V] Title: 아스키 코드, Time: 172 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+        
+        System.out.println(c+0);
+        sc.close();
+    }
+}
