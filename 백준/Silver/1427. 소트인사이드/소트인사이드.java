f1144b3abfe14abfb52e642920d806c9ac7717c8
w00jinLee <122732558+w00jinLee@users.noreply.github.com>
2025-06-26 19:07:40 +0900

    [Silver V] Title: 소트인사이드, Time: 220 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		char[] arr = sc.nextLine().toCharArray();
+		
+		Arrays.sort(arr);
+		
+		for(int i= arr.length-1; i>=0; i--) {
+			System.out.print(arr[i]);
+		}
+	}
+}
