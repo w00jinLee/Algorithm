d27254fdcc1fe6149e910c46c9ecd4c135955fc2
w00jinLee <122732558+w00jinLee@users.noreply.github.com>
2026-03-08 14:00:43 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for i in range(T):
+    str=""
+
+    inputs = input().split() # ["반복횟수", "문자열" 로 저장]
+    inputs[0] = int(inputs[0])
+
+    for i in range(len(inputs[1])):
+        for j in range(inputs[0]):
+            str += inputs[1][i]
+
+    print(str)       
