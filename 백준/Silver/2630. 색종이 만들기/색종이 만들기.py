8a1104943c1924f81501bf79a25afe6c40814c04
w00jinLee <122732558+w00jinLee@users.noreply.github.com>
2026-03-17 11:54:11 +0900

    [Silver II] Title: 색종이 만들기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -0,0 +1,40 @@
+import sys
+
+N = int(input())
+paper=[]
+for _ in range(N):
+    paper.append(list(map(int, sys.stdin.readline().strip().split())))
+
+blueCount = 0
+whiteCount = 0
+
+def cutPaper(y, x, paper_len):
+    global blueCount, whiteCount
+    # 파란색 종이일 때
+    if paper[y][x] == 1:
+        for i in range(y, y + paper_len):
+            for j in range(x, x + paper_len):
+                if paper[i][j] == 0:
+                    cutPaper(y + paper_len//2, x + paper_len//2, paper_len//2)
+                    cutPaper(y + paper_len//2, x, paper_len//2)
+                    cutPaper(y, x + paper_len//2, paper_len//2)
+                    cutPaper(y, x, paper_len//2)
+                    return
+        blueCount +=1
+
+    # 흰색 종이일 때                
+    else : 
+        for i in range(y, y + paper_len):
+            for j in range(x, x + paper_len):
+                if paper[i][j] == 1:
+                    cutPaper(y + paper_len//2, x + paper_len//2, paper_len//2)
+                    cutPaper(y + paper_len//2, x, paper_len//2)
+                    cutPaper(y, x + paper_len//2, paper_len//2)
+                    cutPaper(y, x, paper_len//2)
+                    return
+        whiteCount +=1
+
+cutPaper(0, 0, N)
+
+print(whiteCount)
+print(blueCount)
