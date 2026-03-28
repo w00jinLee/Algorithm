fed7045bdaa6971ac29f8299b8fc25614abd3a7b
w00jinLee <122732558+w00jinLee@users.noreply.github.com>
2026-03-28 16:51:34 +0900

    [Silver I] Title: 신입 사원, Time: 2104 ms, Memory: 37048 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,21 +1,44 @@
+# import sys
+
+# T = int(input())
+
+# for _ in range(T):
+#     N = int(input())
+#     passCount = 0
+#     peopleList = []
+    
+#     for i in range(N):
+#         peopleList.append(tuple(map(int, sys.stdin.readline().split())))
+
+#     peopleList.sort()
+#     minMeeting = 100001
+
+#     for i in range(len(peopleList)):
+#         if peopleList[i][1] < minMeeting:
+#             minMeeting = peopleList[i][1]
+#             passCount+=1
+
+#     print(passCount)
+
+
 import sys
 
-T = int(input())
+T = int(sys.stdin.readline())
 
 for _ in range(T):
-    N = int(input())
+    N = int(sys.stdin.readline())
     passCount = 0
-    peopleList = []
+    peopleList = [0]*(N+1)
     
     for i in range(N):
-        peopleList.append(tuple(map(int, sys.stdin.readline().split())))
+        a, b = map(int, sys.stdin.readline().split())
+        peopleList[a]=b
 
-    peopleList.sort()
     minMeeting = 100001
-
-    for i in range(len(peopleList)):
-        if peopleList[i][1] < minMeeting:
-            minMeeting = peopleList[i][1]
+    
+    for i in range(1, len(peopleList)):
+        if peopleList[i] < minMeeting:
+            minMeeting = peopleList[i]
             passCount+=1
 
     print(passCount)
